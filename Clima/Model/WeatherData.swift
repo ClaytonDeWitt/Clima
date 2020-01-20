6054950f560f6751e84309298c48154383ea15af
Clayton DeWitt <cdnp44@gmail.com>
2020-01-19 18:42:11 -0800

    First commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Clay on 11/18/19.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+
+struct Main: Codable {
+    let temp: Double
+}
+
+
+struct Weather: Codable {
+    let id: Int
+}
